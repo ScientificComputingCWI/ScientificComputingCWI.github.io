80c894a7bacb9ecd5be1339803212f3a2f128061
Syver Døving Agdestein <syverda@gmail.com>
2025-08-28 19:38:04 +0200

    Fix config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -50,26 +50,26 @@ export default defineConfig({
     ],
   },
 
-  // vite: {
-  //   // resolve: {
-  //   //   alias: {
-  //   //     '@': path.resolve(__dirname, '../components')
-  //   //   }
-  //   // },
-  //   optimizeDeps: {
-  //     exclude: [ 
-  //       '@nolebase/vitepress-plugin-enhanced-readabilities/client',
-  //       'vitepress',
-  //       '@nolebase/ui',
-  //     ], 
-  //   }, 
-  //   ssr: { 
-  //     noExternal: [ 
-  //       // If there are other packages that need to be processed by Vite, you can add them here.
-  //       '@nolebase/vitepress-plugin-enhanced-readabilities',
-  //       '@nolebase/ui',
-  //     ], 
-  //   },
-  // },
+  vite: {
+    // resolve: {
+    //   alias: {
+    //     '@': path.resolve(__dirname, '../components')
+    //   }
+    // },
+    optimizeDeps: {
+      exclude: [ 
+        '@nolebase/vitepress-plugin-enhanced-readabilities/client',
+        'vitepress',
+        '@nolebase/ui',
+      ], 
+    }, 
+    ssr: { 
+      noExternal: [ 
+        // If there are other packages that need to be processed by Vite, you can add them here.
+        '@nolebase/vitepress-plugin-enhanced-readabilities',
+        '@nolebase/ui',
+      ], 
+    },
+  },
 
 })
